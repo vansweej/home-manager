aec88f63c60741e38585581d15ed60ebe017934a
Jan Van Sweevelt <vansweej@gmail.com>
2026-06-19 11:57:24 +0200

    feat: add nixGL-wrapped foliate e-book reader

--- modules/linux.nix
+++ modules/linux.nix
@@ -30,6 +30,10 @@ in
         obs-nixgl = pkgs.writeShellScriptBin "obs-nixgl" ''
           exec ${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${pkgs.obs-studio}/bin/obs "$@"
         '';
+
+        foliate-nixgl = pkgs.writeShellScriptBin "foliate-nixgl" ''
+          exec ${pkgs.nixgl.nixGLIntel}/bin/nixGLIntel ${pkgs.foliate}/bin/foliate "$@"
+        '';
       in
       {
         home.packages = with pkgs; [
@@ -37,7 +41,23 @@ in
 
           ghostty-nixgl
           obs-nixgl
+          foliate-nixgl
         ];
+
+        # Make Foliate the default handler for the e-book formats its .desktop
+        # entry advertises. References foliate-nixgl.desktop (the id defined
+        # above), not the upstream app-id. xdg.enable (common.nix) generates
+        # ~/.config/mimeapps.list from this.
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "application/epub+zip" = "foliate-nixgl.desktop";
+            "application/x-mobipocket-ebook" = "foliate-nixgl.desktop";
+            "application/vnd.amazon.mobi8-ebook" = "foliate-nixgl.desktop";
+            "application/x-fictionbook+xml" = "foliate-nixgl.desktop";
+            "application/vnd.comicbook+zip" = "foliate-nixgl.desktop";
+          };
+        };
 
         # freedesktop.org .desktop entry so Ghostty appears in application launchers.
         home.file.".local/share/applications/ghostty-nixgl.desktop".text = ''
@@ -67,6 +87,22 @@ in
           Terminal=false
           Categories=Video;AudioVideo;
           StartupWMClass=obs
+        '';
+
+        # freedesktop.org .desktop entry so Foliate appears in application launchers.
+        home.file.".local/share/applications/foliate-nixgl.desktop".text = ''
+          [Desktop Entry]
+          Type=Application
+          Version=1.0
+          Name=Foliate (nixGL)
+          GenericName=E-Book Reader
+          Comment=Simple and modern GTK eBook reader (with nixGL wrapper)
+          Exec=${foliate-nixgl}/bin/foliate-nixgl %U
+          Icon=com.github.johnfactotum.Foliate
+          Terminal=false
+          Categories=Office;Viewer;Literature;
+          MimeType=application/epub+zip;application/x-mobipocket-ebook;application/vnd.amazon.mobi8-ebook;application/x-fictionbook+xml;application/vnd.comicbook+zip;
+          StartupWMClass=com.github.johnfactotum.Foliate
         '';
       }
     ))
